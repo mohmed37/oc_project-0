907915a33a7fb79fed756f0d0fceb7ba3647fe66
behillil mohamed <mbehillil@gmail.com>
2018-09-23 14:39:14 +0200

    I fix this bug inside the Product class. So that the amount between the quantity and the price is incremented well. Test OK.

--- src/main/java/com/lambazon/domain/Product.java
+++ src/main/java/com/lambazon/domain/Product.java
@@ -6,65 +6,59 @@ public class Product {
 	private String name, description, details;
 	private int quantity;
 	private double price;
-
+	private double priceTotal;
 	public Product(int id, int quantity, double price, String name, String description) {
 		setId(id);
 		setQuantity(quantity);
 		setPrice(price);
 		setName(name);
 		setDescription(description);
+		setInventoryPrice( price, quantity);
+	    }
+
+	public Integer getId() { return id;	}
+
+	public void setId(Integer id) {	this.id = id; }
+
+	public String getName() { return name;	}
+
+	public void setName(String name) { this.name = name; }
+
+	public String getDescription() { return description; }
+
+	public void setDescription(String description) { this.description = description;}
+
+	public String getDetails() { return details; }
+
+	public void setDetails(String details) { this.details = details; }
+
+	public int getQuantity() { return quantity;	}
+
+	public void setQuantity(int quantity) { this.quantity = quantity; }
+
+	public double getPrice() { return price; }
+
+	public void setPrice(double price) { this.price = price; }
+
+
+	public double getInventoryPrice()
+	{
+		return priceTotal;
+
 	}
 
-	public Integer getId() {
-		return id;
-	}
+	/**
+	 * Sum of the price and quantity. Integration of Set InventoryPrice.
+	 * @param price
+	 * @param quantity
+	 * @return
+	 */
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public String getDetails() {
-		return details;
-	}
-
-	public void setDetails(String details) {
-		this.details = details;
-	}
-
-	public int getQuantity() {
-		return quantity;
-	}
-
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-
-	public double getPrice() {
-		return price;
-	}
-
-	public void setPrice(double price) {
-		this.price = price;
-	}
-
-	public double getInventoryPrice() {
-		// TODO Auto-generated method stub
-		return 0.0;
+	public double setInventoryPrice(double price, int quantity)
+	{
+		price = getPrice();
+		quantity = getQuantity();
+		priceTotal = price* quantity;
+		return priceTotal;
 	}
 }
